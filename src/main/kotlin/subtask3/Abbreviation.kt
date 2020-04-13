90e527e32a308e7f2d0ef232c0add80ea207294c
Denis Chueshkov <kot_denis@mail.ru>
2020-04-13 23:28:56 +0300

    feat: complete Abbreviation task

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,20 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
+        val itogString = a.toUpperCase()
+        var i = 0
+        var j = 0
+        while (i < itogString.length){
+            if (j < b.length)
+                if (itogString[i] == b[j])
+                    j++
+            i++
+        }
+        if (j == b.length)
+            return "YES"
+        else
+            return "NO"
         throw NotImplementedError("Not implemented")
     }
 }
