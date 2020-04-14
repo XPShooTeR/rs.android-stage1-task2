798ae96d96699149988cf0038c50b707ecb25f60
Denis Chueshkov <kot_denis@mail.ru>
2020-04-14 18:31:04 +0300

    feat: complete blocks task

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,35 @@
 package subtask5
 
 import kotlin.reflect.KClass
+import java.time.LocalDate
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var i = 0
+        var itogSum: Int
+        var itogString = ""
+        var vrem: String
+        val formatDate = java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        var itogDate = LocalDate.parse("01.01.1000", formatDate)
+        itogSum = 0
+        while (i < blockA.size){
+            if ((blockA[i] is String) and (blockB == String::class))
+                itogString += blockA[i]
+            if ((blockA[i] is Int) and (blockB == Int::class)) {
+                vrem = blockA[i].toString()
+                itogSum = itogSum + vrem.toInt()
+            }
+            if ((blockA[i] is LocalDate) and (blockB == LocalDate::class))
+                if (itogDate < LocalDate.parse(blockA[i].toString().substring(8,10)+"."+blockA[i].toString().substring(5,7)+"."+blockA[i].toString().substring(0,4), formatDate))
+                    itogDate = LocalDate.parse(blockA[i].toString().substring(8,10)+"."+blockA[i].toString().substring(5,7)+"."+blockA[i].toString().substring(0,4), formatDate)
+            i++
+        }
+        when (blockB){
+            String::class -> return itogString
+            Int::class -> return itogSum
+            LocalDate::class -> return itogDate.format(formatDate)
+            else -> return ""
+        }
     }
 }
